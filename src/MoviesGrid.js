9a4e430524f9465abb8ffa7d85da8aacfc1fa057
Victor Antonio <victormartinezsolis20@gmail.com>
2022-11-07 22:04:28 -0600

    style: Se agregan estilos a las cards

--- src/MoviesGrid.js
+++ src/MoviesGrid.js
@@ -1,9 +1,10 @@
 import { MovieCard } from "./MovieCard";
 import movies from "./movies.json";
+import styles from "./MoviesGrid.module.css";
 
 export function MoviesGrid() {
   return (
-    <ul>
+    <ul className={styles.moviesGrid}>
       {movies.map((movie) => {
         return <MovieCard key={movie.id} movie={movie}/>
       })}
